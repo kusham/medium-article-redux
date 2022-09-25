367f5af6e3353046ad2e86514b2b0a9ff64b60b1
kusham <kusham1998@gmail.com>
2022-09-25 11:51:57 +0530

    add action types

---
+++ src/constants/ActionType.js
@@ -0,0 +1,7 @@
+export const ADD_TODO = "ADD_TODO";
+export const COMPLETE_TODO = "COMPLETE_TODO";
+export const SHOW_ALL = "SHOW_ALL";
+export const SHOW_ACTIVE = "SHOW_ACTIVE";
+export const SHOW_COMPLETED = "SHOW_COMPLETED";
+export const ADD_TODO_FAIL = "ADD_TODO_FAIL";
+export const ADDING_START = "ADDING_START";
