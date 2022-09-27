9171b97c46aff548c873a3d07a145828bfd0e31e
kusham <kusham1998@gmail.com>
2022-09-27 23:39:50 +0530

    complete the reducer

---
+++ src/reducers/TodoReducers.js
@@ -0,0 +1,19 @@
+import { ADDING_START, ADD_TODO, ADD_TODO_FAIL } from "../constants/ActionType";
+
+const todoReducer = (
+  state = { todoData: [], loading: false, error: false },
+  action
+) => {
+  switch (action.type) {
+    case ADDING_START:
+      return {...state, loading: true};
+    case ADD_TODO:
+      return {...state, todoData: [...state.todoData, action.data], loading: false}
+    case ADD_TODO_FAIL:
+      return {...state, error: true}
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
